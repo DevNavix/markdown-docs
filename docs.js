16cba40674914d0a77cb6de254d57a33d1faa026
Navjot Sharma <navjotpro78@gmail.com>
2025-08-15 12:49:58 +0530

    add: Add body html and run threw js

--- docs.js
+++ docs.js
@@ -4,6 +4,22 @@
  */
 
 // ============================================================================
+// html BODY
+// ============================================================================
+
+// body.js
+fetch("https://devnavix.github.io/markdown-docs/body.html")
+  .then(res => res.text())
+  .then(html => {
+    document.body.innerHTML = html;
+    // Set title from a global variable in index.html
+    document.getElementById("page-title").textContent = window.pageTitle || "Docs";
+    feather.replace();
+  })
+  .catch(err => console.error("Failed to load body.html:", err));
+
+
+// ============================================================================
 // GLOBAL VARIABLES AND INITIALIZATION
 // ============================================================================
 
